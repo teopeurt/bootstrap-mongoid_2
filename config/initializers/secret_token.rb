8f85e444c661a1b9e3ea20475f16bd8d6802d03d
Don Onwunumah <teopeurt@gmail.com>
2012-08-30 15:27:27 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapMongoid2::Application.config.secret_token = 'f7fcccaee9c8441230999617b599bc022f664ed6bab9640287172d1d9ee2c9bdfc1761bb2595c47c1601e45b3cc610dd790cb4fce2f7b73a55cf14409128d272'
